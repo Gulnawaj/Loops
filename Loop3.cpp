1992360c049e919cefe7debc88846f2fc73578d2
Gulnawaj <gulnawaj001646@gmail.com>
2026-05-16 21:55:30 +0530

    Added loop3 program

---
+++ Loop3.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=0; i<=40 ; i++){
+        if(i%4 == 0){
+            cout<<i<<endl;
+        }
+    }
+}
--- tempCodeRunnerFile.cpp
+++
@@ -1,5 +0,0 @@
-char name;
-    for(name = 'a' ; name<= 'z' ; name++){
-        cout<<name<<" ";
-    }
-    cout<<endl;
