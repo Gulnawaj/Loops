0f0501c666c7eb774ded4d96449ce7ef0e8e2ac4
Gulnawaj <gulnawaj001646@gmail.com>
2026-05-27 14:28:16 +0530

    traversing of array

---
+++ Arrays/code01.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+//traversing of array 
+int main(){
+    int arr[5] = {10, 20, 30, 40, 50};
+    for(int i = 0; i<5 ; i++){
+        cout<<arr[i]<<" ";
+    }
+}
